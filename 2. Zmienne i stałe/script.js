c6d6c2874bb9d1beb3a2badf70a431958f53ffd7
Sebastian Krzemiński <72340642+Krzemem@users.noreply.github.com>
2020-11-29 17:40:48 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,22 @@
+zad 1
+let number1 = 2;
+let text1 = "text";
+let numberAndString = 2 + "dwa";
+let logic = 3 < 2;
+let special = null;
+
+console.log(number1);
+console.log(text1);
+console.log(numberAndString);
+console.log(logic);
+console.log(special);
+
+zad2 
+var numberOne = 5;
+var numberTwo = 3;
+var sum = 0;
+sum = numberOne + numberTwo;
+console.log(sum);
+zad 3
+let test;
+console.log(test);
